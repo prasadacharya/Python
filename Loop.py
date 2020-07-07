e433fb8c24178cc2b9a9e639fceabd384bbabc00
prasadacharya <prasada@mail.usf.edu>
2020-07-07 17:33:07 -0400

    Loop

--- Loop.py
+++ Loop.py
@@ -17,16 +17,23 @@ while True:
     print(user_input)
 print('end of loop 2')
 
-print('For loop 1')
+print('***For loop 1***')
 for i in [1,2,3,4,5,6]:
     print(i)
 
-print('For loop 2')
+print('***For loop 2***')
 friends = ['harry potter', 'batman', 'joker']
-
 for friend in friends:
     print('Happy birthday', friend)
 
+print('***For loop 3***')
+large = -1
+items = [23,67,25,88,45]
 
+for i in items:
+    if i>large:
+        large=i
+
+print('largest number: ', large)
 
 
