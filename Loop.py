30aa8e607d6dd2f914599dbb4775bf3f2addd7ea
prasadacharya <prasada@mail.usf.edu>
2020-07-09 17:50:46 -0400

    Loop

--- Loop.py
+++ Loop.py
@@ -36,4 +36,62 @@ for i in items:
 
 print('largest number: ', large)
 
+print('***For loop 4***')
+count = 0
 
+for item in [10, 20, 30, 40, 50, 60, 70, 80]:
+    count = count + 1
+print('Number of items in array:', count)
+
+
+print('***For loop 5***')
+sum = 0
+
+for item in [10, 20, 30, 40, 50, 60, 70, 80]:
+    sum = sum + item
+print('Sum of all items in array:', sum)
+
+print('***For loop 6***')
+count = 0
+sum = 0
+avg = 0
+
+for item in [10, 20, 30, 40, 50, 60, 70, 80]:
+    count = count + 1
+    sum = sum + item
+
+avg = sum/count
+print('avg of all items in array:', avg)
+
+
+print('***For loop 7***')
+
+arr = [10, 20, 30, 40, 50, 60, 70, 80]
+
+for item in arr:
+   if item>50:
+       print('Large number',item)
+
+print('***For loop 7***')
+
+arr = [10, 20, 30, 40, 50, 60, 70, 80]
+found=False
+for item in arr:
+   if item==50:
+       found = True
+       break
+
+print ('Found', found)
+
+print('***For loop 8***')
+
+arr = [100, 10, 20, 30, 40, 10, 5, 50, 60, 70, 80]
+small= arr[0]
+
+
+for item in arr:
+   if item < small:
+       small = item
+
+
+print ('smallest', small)
